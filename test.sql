8f36830e63084543807b0456ef3bc5ac78cb55b5
Minh Nguyen <mtheninja@gmail.com>
2023-01-05 08:00:03 -0600

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,6 +1,4 @@
 use database AUTO_DEMO_TDDB;
 use schema PUBLIC;
 use warehouse AUTO_DEMO_TDDB_WH;
-select * from mlb_player_stats_batters
-where team = 'CHC'
-and player like '%Jake%'
+select * from mlb_player_stats_batters where team = 'CHC' and player like '%Jake%'
